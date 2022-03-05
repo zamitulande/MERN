09acec8bc49a3ae69c08c1effb5a1982d62aa5b5
zami tulande <hulmartulande@gmail.com>
2022-03-05 11:29:04 -0500

    inicio backend

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+ import express  from "express"
+ import dotenv from "dotenv"
+ import conectarDB from "./config/db.js"
+
+ const app = express() 
+ dotenv.config()
+ conectarDB();
+
+const PORT = process.env.PORT || 4000;
+
+ app.listen(PORT, ()=>{
+     console.log(`servidor en el puerto ${PORT}`)
+ })
