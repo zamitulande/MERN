9acbaaa940489847c0d97ff573d1c3ef6f7650b4
zami tulande <hulmartulande@gmail.com>
2022-03-09 10:31:29 -0500

    generar JWT

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -1,5 +1,6 @@
 import Usuario from '../models/Usuario.js'
 import generarId from '../helpers/generarId.js';
+import generarJWT from '../helpers/generarJWT.js';
 
 const registrar = async (req, res)=>{
 
@@ -42,6 +43,7 @@ const autenticar = async (req, res)=>{
            _id : usuario._id,
            nomre : usuario.nombre,
            email : usuario.email,
+           token : generarJWT(usuario._id),
        })
     }else{
         const error = new Error('El password es incorrrecto');
