9237f55662428098b4c51cd1bb1f0224208d8fab
zami tulande <hulmartulande@gmail.com>
2022-03-10 10:48:49 -0500

    validar token para cambiar password

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -65,5 +65,31 @@ const confirmar = async (req, res)=>{
    } catch (error) {
        console.log(error);
    }
+};
+const olvidePassword = async (req, res)=>{
+    const {email}= req.body;
+    const usuario = await Usuario.findOne({email});
+    if(!usuario){
+        const error = new Error('El usuario no existe');
+        return res.status(404).json({msg: error.message});
+    }
+    try {
+        usuario.token = generarId();
+        await usuario.save();
+        res.json({msg: 'Se ha enviado un correo con instrucciones'})
+    } catch (error) {
+        console.log(error)
+    }
+};
+const comprobarToken = async  (req, res)=>{
+    const {token} = req.params;
+
+    const tokenValido = await Usuario.findOne({token});
+    if(tokenValido){
+       res.json({msg : 'token valido, El usuario si existe'})
+    }else{
+        const error = new Error('Token no valido');
+        return res.status(404).json({msg: error.message});
+    }
 }
-export {registrar, autenticar, confirmar};
+export {registrar, autenticar, confirmar, olvidePassword, comprobarToken};
--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -1,10 +1,12 @@
-import express, { Router } from "express";
+import express from "express";
 const router = express.Router();
-import {registrar, autenticar, confirmar} from '../controllers/usuarioController.js'
+import {registrar, autenticar, confirmar, olvidePassword,comprobarToken} from '../controllers/usuarioController.js'
 
 //AUTENTICACION, REGISTRO Y CONFIRMACION DE USUARIOS
 router.post('/', registrar); //crea un nuevo usuario
 router.post('/login', autenticar);
 router.get('/confirmar/:token', confirmar);
+router.post('/olvide-password', olvidePassword);
+router.get('/olvide-password/:token', comprobarToken)
 
 export default router;
