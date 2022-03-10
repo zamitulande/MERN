949d61ebe3221ea9271070c9075eb9b84e488d25
zami tulande <hulmartulande@gmail.com>
2022-03-10 12:27:05 -0500

    cambiando password

--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -1,12 +1,13 @@
 import express from "express";
 const router = express.Router();
-import {registrar, autenticar, confirmar, olvidePassword,comprobarToken} from '../controllers/usuarioController.js'
+import {registrar, autenticar, confirmar, olvidePassword,comprobarToken, nuevoPassword} from '../controllers/usuarioController.js'
 
 //AUTENTICACION, REGISTRO Y CONFIRMACION DE USUARIOS
 router.post('/', registrar); //crea un nuevo usuario
 router.post('/login', autenticar);
 router.get('/confirmar/:token', confirmar);
 router.post('/olvide-password', olvidePassword);
-router.get('/olvide-password/:token', comprobarToken)
+
+router.route('/olvide-password/:token').get(comprobarToken).post(nuevoPassword);
 
 export default router;
